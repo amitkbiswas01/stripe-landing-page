9b6fb6b6d1796233b7faf7ceeea64abe7ab4ca07
amitkbiswas01 <amitkbiswas01@gmail.com>
2023-06-21 17:23:36 +0600

    add: gradient animation related code update: page template

--- web/themes/custom/stripe_landing/js/main.js
+++ web/themes/custom/stripe_landing/js/main.js
@@ -1,7 +1,11 @@
+// css
 import "../css/style.scss";
 
-(($, Drupal) => {
-  Drupal.behaviors.showText = {
-    attach: function (context, settings) {},
-  };
-})(jQuery, Drupal);
+// js
+import "../components/gradient/gradient";
+
+// (($, Drupal) => {
+//   Drupal.behaviors.showText = {
+//     attach: function (context, settings) {},
+//   };
+// })(jQuery, Drupal);
